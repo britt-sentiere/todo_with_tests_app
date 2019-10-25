a015a6570b72dcb97db2abf408d5d29b49169dce
brittani <britt.sentiere@gmail.com>
2019-10-25 10:58:58 -0500

    refactored list.rb and test code

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,36 +6,44 @@ class List < ApplicationRecord
     end
 
     def snooze_all_tasks!
-      tasks.each do |task|
-        task.snooze_hour!
+      # tasks.each do |task|
+      #   task.snooze_hour!
+
+      tasks.each { |task| task.snooze_hour! }
       end
     end
 
     def total_duration
-      total = 0
-      tasks.each do |task|
-        total += task.duration
-      end
-      return total
+      # total = 0
+      # tasks.each do |task|
+      #   total += task.duration
+      # end
+      # return total
+
+      tasks.sum { |task| task.duration }
     end
 
     def incomplete_tasks
-      array_of_tasks = []
-      tasks.each do |task|
-        if !task.complete
-          array_of_tasks << task
-        end
-      end
-      return array_of_tasks
+    #   array_of_tasks = []
+    #   tasks.each do |task|
+    #     if !task.complete
+    #       array_of_tasks << task
+    #     end
+    #   end
+    #   return array_of_tasks
     end
 
+    tasks.reject { |task| task.complete }
+
     def favorite_tasks
-      array_of_tasks = []
-      tasks.each do |task|
-        if task.favorite
-          array_of_tasks << task
-        end
-      end
-      return array_of_tasks
+      # array_of_tasks = []
+      # tasks.each do |task|
+      #   if task.favorite
+      #     array_of_tasks << task
+      #   end
+      # end
+      # return array_of_tasks
+
+      tasks.select { |task| task.favorite }
     end
 end
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -15,62 +15,71 @@ RSpec.describe List, type: :model do
     end
   end
 
-  describe '#snooze_all_tasks!' do 
-    it 'should delay all tasks by an hour' do
-      moment_in_time = Time.now
-      list = List.create(name: "Homework")
-      Task.create(list_id: list.id, deadline: moment_in_time)
-      Task.create(list_id: list.id, deadline: moment_in_time)
+ describe '#snooze_all_tasks!' do 
+     it 'should snooze each task on a list' do 
+       list = List.create(name: "Homework")
 
-      list.snooze_all_tasks!
+       moments_in_time = [Time.now, 36.minutes.from_now, 2.hours.ago]
 
-      list.tasks.each do |task|
-        expect(task.deadline).to eq(moment_in_time + 1.hour)
-      end 
-    end
-  end
+       moments_in_time.each do |moment_in_time|
+         Task.create(list_id: list.id, deadline: moment_in_time)
+       end
+
+       list.snooze_all_tasks!
+
+       list.tasks.order(:id).each.with_index do |task, index|
+         expect(task.deadline).to eq(moments_in_time[index] + 1.hour)
+       end
+     end
+   end
 
   describe '#total_duration' do
-    it 'should display the time necessary to complete the task' do
+    it 'should display the time necessary to complete the tasks' do
 
       list = List.create(name: "Cleaning")
       Task.create(list_id: list.id, duration: 30)
       Task.create(list_id: list.id, duration: 30)
       Task.create(list_id: list.id, duration: 30)
 
-      list.total_duration
 
         expect(list.total_duration).to eq(90)
     end
   end 
 
-  describe '#incomplete_tasks' do
-    it 'should display incomplete_tasks' do
-      list = List.create(name: "Shopping")
-      Task.create(list_id: list.id, complete: false)
-      Task.create(list_id: list.id, complete: false)
 
-      list.incomplete_tasks
+   describe '#incomplete_tasks' do 
+      it 'should return a collection of the tasks on this list that are incomplete' do
+        list = List.create(name: "Shopping")
+        task_1 = Task.create(list_id: list.id, complete: true)
+        task_2 = Task.create(list_id: list.id, complete: false)
+        task_3 = Task.create(list_id: list.id, complete: false)
+        task_4 = Task.create(list_id: list.id, complete: true)
 
-      list.tasks.each do |task| 
-        expect(task.complete).to eq(false)
+        expect(list.incomplete_tasks).to match_array([task_3, task_2])
+        expect(list.incomplete_tasks.count).to eq(2)
+
+        list.incomplete_tasks.each do |task|
+          expect(task.complete).to eq(false)
+        end
+      end
+    end
+
+    describe '#favorite_tasks' do 
+      it 'should return a collection of the tasks on this list that are favorited' do
+        list = List.create(name: "Yard Work")
+        task_1 = Task.create(list_id: list.id, favorite: true)
+        task_2 = Task.create(list_id: list.id, favorite: false)
+        task_3 = Task.create(list_id: list.id, favorite: true)
+        task_4 = Task.create(list_id: list.id, favorite: false)
+
+        testing_collection = list.favorite_tasks
+
+        expect(testing_collection).to match_array([task_1, task_3])
+        expect(testing_collection.count).to eq(2)
+
+        testing_collection.each do |task|
+          expect(task.favorite).to eq(true)
+        end
       end
     end
   end
-
-  describe '#favorite_tasks' do
-    it 'should select the favorite tasks' do
-      list = List.create(name: "Yard Work")
-      Task.create(list_id: list.id, favorite: true)
-      Task.create(list_id: list.id, favorite: true)
-      Task.create(list_id: list.id, favorite: true)
-
-      list.favorite_tasks
-
-      list.tasks.each do |task|
-        expect(task.favorite).to eq(true)
-      end
-    end
-  end
-end
-
